b90c6736827293fe64636120c0c3d8c45028f950
Alex-Taylor Danko <alexdankoxxl@gmail.com>
2022-11-06 13:13:34 +0200

    fix name MyInput

--- src/components/UI/index.js
+++ src/components/UI/index.js
@@ -1,5 +1,5 @@
 import MyButton from "./MyButton.vue";
-import MyInput from "./MyInputI.vue";
+import MyInput from "./MyInput.vue";
 import MyModal from "./MyModal.vue";
 import MySelect from "./MySelect.vue";
 

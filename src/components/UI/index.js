e8848f03ebe278a278be188c443d49ba943cbb7a
Alex-Taylor Danko <alexdankoxxl@gmail.com>
2022-11-06 18:27:49 +0200

    delete MyContainer

--- src/components/UI/index.js
+++ src/components/UI/index.js
@@ -1,6 +1,7 @@
+import MyContainer from "./MyContainer.vue";
 import MyButton from "./MyButton.vue";
 import MyInput from "./MyInput.vue";
 import MyModal from "./MyModal.vue";
 import MySelect from "./MySelect.vue";
 
-export default [MyButton, MyInput, MyModal, MySelect];
+export default [MyContainer, MyButton, MyInput, MyModal, MySelect];
